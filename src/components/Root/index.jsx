cbd9f6286c5d4bfa9e6c2e8c186f7fb850cc8d53
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2018-03-20 18:17:20 -0400

    change .js to .jsx for files containing JSX

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 
-const Root = ({ store }) =>
+const Root = ({ store }) => (
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>;
+  </Provider>
+);
 
 Root.propTypes = {
   store: PropTypes.object.isRequired,
